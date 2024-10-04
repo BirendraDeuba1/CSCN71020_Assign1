0319481dca9140bafa32282c50392c2db5bd2406
Birendra Deuba <Bdeuba7977@conestogac.on.ca>
2024-10-04 00:41:24 -0400

    Revert "commit #3 fixed the subtraction operation and included the user input"
    This reverts commit 030b07e55fd4bec66dd38671a05f75ae3b0c330c.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,9 +3,9 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-void subtract();
 
-int main() {
+
+void main() {
 
 	printWelcomeMenu();
 
@@ -14,19 +14,14 @@ int main() {
 	int inputNum;
 
 	printf("Enter operation number: ");
-	scanf_s("%d", &inputNum);
+	scanf_s("%1o", &inputNum);
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
-
-	case 2:
-		subtract();
-
 	}
 
-	return 0;
 }
 
 void printWelcomeMenu() {
@@ -49,14 +44,4 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
-}
-
-void subtract() {
-	double num1, num2, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	result = num1 - num2;
-	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
